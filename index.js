6d703925c24eca7b8b1adbc5022f61d89f21624e
Patrick Obrtal <patrick.obrtal@student.tuke.sk>
2022-06-12 12:30:25 +0200

    Version 1.2

--- index.js
+++ index.js
@@ -1,20 +1,59 @@
-let myLeads = []
+let mySites = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const delBtn = document.getElementById("del-btn")
+const saveBtn = document.getElementById("save-btn")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
-})
+if (leadsFromLocalStorage) {
+    mySites = leadsFromLocalStorage
+    render(mySites)
+}
 
-
-function renderLeads() {
+function render(sites) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+    for (let i = 0; i < sites.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${sites[i]}'>
+                    ${sites[i]}
+                </a>
+            </li>
+    `
     }
     ulEl.innerHTML = listItems
 }
+
+inputBtn.addEventListener("click", function() {
+    mySites.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(mySites))
+    render(mySites)
+})
+
+inputEl.addEventListener("keypress", (event)=> {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        mySites.push(inputEl.value)
+        inputEl.value = ""
+        render(mySites)
+    }
+})
+
+saveBtn.addEventListener("click", function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        mySites.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(mySites))
+        render(mySites)
+    })
+})
+
+delBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    while (mySites.length > 0) {
+        mySites.pop()
+    }
+    render(mySites)
+})
 
